a817909dd75ba8f106212d1f7cfbd30ce0bce8bf
veljkoid <99028355+veljkoid@users.noreply.github.com>
2023-08-28 16:23:23 +0200

    abdejtovan so.py

--- so.py
+++ so.py
@@ -1,6 +1,7 @@
 f = open("initOutput.txt")
 code  = f.readline()
 output = [[char, 1] for char in code]
+g= open("code.txt", 'w')
 for element in output:
-  f.write(element[0] + ' ' + element[1] + '\n')
-f.close()
+  g.write(str(element[0]) + ' ' + str(element[1]) + '\n')
+g.close()
