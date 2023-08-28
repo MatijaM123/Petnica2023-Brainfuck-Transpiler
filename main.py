d20f4b0100653b6067f547a6b30880e2e28d814e
veljkoid <99028355+veljkoid@users.noreply.github.com>
2023-08-28 16:52:17 +0200

    abdejtovan main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from os import write
 import numpy as np
-putanja = input()
+putanja = 'code.txt'
 
 
 with open(putanja, "r") as rf:
-    with open(putanja, "w") as wf:
-        linija = rf.readline()
+    with open('output.py', "w") as wf:
+        linije = rf.readlines()
+        linija = [list(map(str, linije[i].split())) for i in range len(linije)]
         i = 0
         uvlacenje = 0
         wf.write("import numpy as np\nniz = np.zeros(30000, dtype=chr)\n")
         for komanda in linija:
-            match komanda:
+            match komanda[0]:
                 case '>':
-                    wf.write(uvlacenje*"\t"+"i += 1\n")
+                    wf.write(uvlacenje*"\t"+"i +="+komanda[1]+"n")
                 case '<':
-                    wf.write(uvlacenje*"\t"+"i -= 1\n")
+                    wf.write(uvlacenje*"\t"+"i -="+komanda[1]+"\n")
                 case '+':
-                    wf.write(uvlacenje*"\t"+"niz[i] += 1\n")
+                    wf.write(uvlacenje*"\t"+"niz[i] +="+komanda[1]+"\n")
                 case '-':
-                    wf.write(uvlacenje*"\t"+"niz[i] -= 1\n")
+                    wf.write(uvlacenje*"\t"+"niz[i] -="+komanda[1]+"\n")
                 case '.':
                     wf.write(uvlacenje*"\t"+"print(niz[i])\n")
                 case ',':
