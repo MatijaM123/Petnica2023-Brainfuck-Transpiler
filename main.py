a132a2675a584b5a549d2b83bfe8da8b1f9098a6
veljkoid <99028355+veljkoid@users.noreply.github.com>
2023-08-28 17:17:44 +0200

    abdejtovan main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ putanja = 'code.txt'
 with open(putanja, "r") as rf:
     with open('output.py', "w") as wf:
         linije = rf.readlines()
-        linija = [list(map(str, linije[i].split())) for i in range len(linije)]
+        linija = [list(map(str, linije[i].split())) for i in range (len(linije))]
         i = 0
         uvlacenje = 0
-        wf.write("import numpy as np\nniz = np.zeros(30000, dtype=chr)\n")
+        wf.write("import numpy as np\nniz = np.zeros(30000, dtype=int)\ni=0\n")
         for komanda in linija:
             match komanda[0]:
                 case '>':
-                    wf.write(uvlacenje*"\t"+"i +="+komanda[1]+"n")
+                    wf.write(uvlacenje*"\t"+"i +="+komanda[1]+"\n")
                 case '<':
                     wf.write(uvlacenje*"\t"+"i -="+komanda[1]+"\n")
                 case '+':
@@ -21,7 +21,7 @@ with open(putanja, "r") as rf:
                 case '-':
                     wf.write(uvlacenje*"\t"+"niz[i] -="+komanda[1]+"\n")
                 case '.':
-                    wf.write(uvlacenje*"\t"+"print(niz[i])\n")
+                    wf.write(uvlacenje*"\t"+"print(chr(niz[i]), end = '')\n")
                 case ',':
                     wf.write(uvlacenje*"\t"+"niz[i]=ord(input())\n")
                 case '[':
@@ -29,7 +29,6 @@ with open(putanja, "r") as rf:
                     uvlacenje+=1
                 case ']':
                     uvlacenje -= 1
-                    
+        wf.write("print(\n)")            
             
-
 
