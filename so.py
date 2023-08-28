96adbde3495b51c10021d99aedd629c7b541b8a9
veljkoid <99028355+veljkoid@users.noreply.github.com>
2023-08-28 17:21:42 +0200

    uklonjen so.py jer postoji u code

--- so.py
+++
@@ -1,7 +0,0 @@
-f = open("initOutput.txt")
-code  = f.readline()
-output = [[char, 1] for char in code]
-g= open("code.txt", 'w')
-for element in output:
-  g.write(str(element[0]) + ' ' + str(element[1]) + '\n')
-g.close()
