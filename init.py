c82cd65d8d3e88d5c213d6bcf1af732065a7226b
Holy-Fork0 <124525942+Holy-Fork0@users.noreply.github.com>
2023-08-28 16:10:21 +0200

    Napravljen init.py

---
+++ init.py
@@ -0,0 +1,11 @@
+operators = ['<', '>', '+', '-', '[', ']']
+code = ''
+
+with open('Brainfuck.bf') as file:
+  line = file.readline()
+  for char in line:
+    if char in operators:
+      code += char
+
+f = open('initOutput.txt', 'w')
+f.write(code)
