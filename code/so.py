a12efd57a454270d5384c9cb685af4046ff39a5d
veljkoid <99028355+veljkoid@users.noreply.github.com>
2023-08-29 23:38:33 +0200

    Update so.py
    Optimisation, double checking for colliding instructions

--- code/so.py
+++ code/so.py
@@ -1,36 +1,51 @@
+import copy
+
 f = open("initOutput.txt")
 code  = f.readline()
 output = []
 operacije = ['+', '-', '<', '>']
-suprotni = [['+', '-'], ['<', '>']]
+suprotni = [['+', '-'], ['<', '>'], ['-', '+'], ['>', '<']]
 i = 0
-while i<len(code):
-  z = 0
-  if(code[i]=='+' or code[i] == '-' or code[i]=='>' or code[i] == '<'):
-    p = i
-    while i<len(code) and abs(ord(code[p]) - ord(code[i])) <= 2:
-      if(code[p]==code[i]):       
-        z+=1
-      else:       
-        z-=1
-      i+=1
-    if(z>0):
-      output.append([code[p],z])
-    elif(z<0):
-      match code[p]:
-        case '>':
-          output.append(['<',-z])
-        case '<':
-          output.append(['>',-z])
-        case '+':
-          output.append(['-',-z])
-        case '-':
-          output.append(['+',-z])
-  else:
-    output.append([code[i], 1])
-    i+=1
+preth = ''
+for i in range(2):
+    output=[]
+    for char in code:
+        if char == preth and char in operacije:
+            output[-1][1]+=1
+        else:
+            output.append([char, 1])
+            preth = char
+    stvar = []
+    i=0
+    for element in output[1:]:
+        stvar = []
+        stvar.append(output[output.index(element)-1][0])
+        stvar.append(element[0])
+        if stvar in suprotni:
+            element[1]-=output[output.index(element)-1][1]
+            output.pop(i)
+        else:
+            i+=1  
+    
+    for element in output:
+        if element[1]==0:
+            output.remove(element)
 
-g= open("code.txt", 'w')
+    for element in output:
+        if element[1]<0:
+            element[1]=abs(element[1])
+            if element[0] == '<':
+                element[0] = '>'
+            elif element[0] == '<':
+                element[0] = '>'
+            elif element[0] == '+':
+                element[0] = '-'
+            elif element[0] == '-':
+                element[0] = '+'
+    
+    code = ''.join([element[1]*element[0] for element in output])
+
+f = open("code.txt", 'w')
 for element in output:
-  g.write(str(element[0]) + ' ' + str(element[1]) + '\n')
-g.close()
+    f.write(str(element[0])+" "+str(element[1])+'\n')
+
