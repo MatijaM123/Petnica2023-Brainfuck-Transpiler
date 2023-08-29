0440793075a5b0a98d9f55c23be9470e4633061d
veljkoid <99028355+veljkoid@users.noreply.github.com>
2023-08-29 13:37:34 +0200

    Update so.py\

--- code/so.py
+++ code/so.py
@@ -1,14 +1,24 @@
+def find_sub_list(sl,l):
+    results=[]
+    sll=len(sl)
+    for ind in (i for i,e in enumerate(l) if e==sl[0]):
+        if l[ind:ind+sll]==sl:
+            results.append((ind,ind+sll-1))
+
+    return results
+
 f = open("initOutput.txt")
 code  = f.readline()
 output = []
-for i in range(len(code())):
+i = 0
+while i<len(code):
+  z = 0
   if(code[i]=='+' or code[i] == '-' or code[i]=='>' or code[i] == '<'):
-    z = 1
     p = i
-    while abs(code[p] - code [i]) == 2:
-      if(code[p]==code[i]):
+    while i<len(code) and abs(ord(code[p]) - ord(code[i])) <= 2:
+      if(code[p]==code[i]):       
         z+=1
-      else:
+      else:       
         z-=1
       i+=1
     if(z>0):
@@ -25,11 +35,12 @@ for i in range(len(code())):
           output.append(['+',-z])
   else:
     output.append([code[i], 1])
+    i+=1
 
-    
-    
+nule = find_sub_list([['[', 1], ['-', 1], [']', 1]], output)
+for indeksi in nule:
+    output[indeksi[0]:(indeksi[1]+1)] = [['0', 1]]
 
-output = [[char, 1] for char in code]
 g= open("code.txt", 'w')
 for element in output:
   g.write(str(element[0]) + ' ' + str(element[1]) + '\n')
