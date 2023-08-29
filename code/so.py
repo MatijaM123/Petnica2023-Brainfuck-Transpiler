beac1082df789cdafc5a757d1d0e36fb87d778ad
Matija M <bombmatija@gmail.com>
2023-08-29 11:26:49 +0200

    Transpajler unapredjen tako da vise ne radi istovetne komande koje su napisane u BrainFuck kodu vec blokove istih ili komandi istog tipa sakuplja u jednu komandu. Izmena rađena u so.py fajlu.

--- code/so.py
+++ code/so.py
@@ -1,5 +1,34 @@
 f = open("initOutput.txt")
 code  = f.readline()
+output = []
+for i in range(len(code())):
+  if(code[i]=='+' or code[i] == '-' or code[i]=='>' or code[i] == '<'):
+    z = 1
+    p = i
+    while abs(code[p] - code [i]) == 2:
+      if(code[p]==code[i]):
+        z+=1
+      else:
+        z-=1
+      i+=1
+    if(z>0):
+      output.append([code[p],z])
+    elif(z<0):
+      match code[p]:
+        case '>':
+          output.append(['<',-z])
+        case '<':
+          output.append(['>',-z])
+        case '+':
+          output.append(['-',-z])
+        case '-':
+          output.append(['+',-z])
+  else:
+    output.append([code[i], 1])
+
+    
+    
+
 output = [[char, 1] for char in code]
 g= open("code.txt", 'w')
 for element in output:
